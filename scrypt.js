36b6bf64c767c6c2f532a3571a6bc67205b15f67
EDUARDO MACHADO FERLA <eduardo.ferla@alunoescolassociais.org.br>
2024-10-01 07:21:33 -0300

    portifolio

---
+++ scrypt.js
@@ -0,0 +1,4 @@
+function mostrarWhatsApp() {
+    const contatoWats = document.querySelector('.contato-wats');
+    contatoWats.style.display = contatoWats.style.display === 'none' ? 'block' : 'none';
+}
